4842b469857e25b4d9718c4a98cdced7f9c3679b
olvsivkov <1000listov@mail.ru>
2023-12-16 01:00:32 +0300

    create project

---
+++ src/components/itemRow.jsx
@@ -0,0 +1,14 @@
+function ItemRow(){
+  return (
+    <div className="item-wrapper">
+      <div className="question-block"></div>
+      <div className="answer-block">
+        <div className="team-one"></div>
+        <div className="false"></div>
+        <div className="team-two"></div>
+      </div>
+    </div>
+  )
+}
+
+export {ItemRow}
---
+++ src/components/main.jsx
@@ -0,0 +1,16 @@
+import { ItemRow } from "./itemRow"
+
+function Main() {
+  return (
+    <div className="main-wrapper">
+      <ItemRow/>
+      <ItemRow/>
+      <ItemRow/>
+      <ItemRow/>
+      <ItemRow/>
+      <ItemRow/>
+    </div>
+  )
+}
+
+export {Main}
